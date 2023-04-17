9fd667b28646b956949e2ae4a2a2b62fbccb81c1
SeungJoonChoi <tmdwns0845@naver.com>
2023-04-17 21:02:26 +0900

    .

--- EchoServer.hpp
+++ EchoServer.hpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define BUF_SIZE 10
 
@@ -28,73 +29,75 @@ public:
 
     void serverStart(int port)
     {
-        int serverSocket, clientSocket;
-        struct sockaddr_in serverAddress, clientAddress;
+        int listenSock, clientSock;
+        struct sockaddr_in listenAddr, clientAddr;
         struct timeval timeout;
 
-        fd_set clients, cpy_clients;
+        fd_set fds, readSet, writeSet;
 
         socklen_t adr_sz;
         int fd_max, fd_num, nread;
         char buf[BUF_SIZE];
 
-        serverSocket = socket(PF_INET, SOCK_STREAM, 0);
-        ft_bzero(&serverAddress, sizeof(serverAddress));
-        serverAddress.sin_family = AF_INET;
-        serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-        serverAddress.sin_port = htons(port);
+        listenSock = socket(PF_INET, SOCK_STREAM, 0);
+        ft_bzero(&listenAddr, sizeof(listenAddr));
+        listenAddr.sin_family = AF_INET;
+        listenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        listenAddr.sin_port = htons(port);
 
-        bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-        listen(serverSocket, 5);
+        bind(listenSock, (struct sockaddr*)&listenAddr, sizeof(listenAddr));
+        listen(listenSock, 5);
 
-        FD_ZERO(&clients);
-        FD_SET(serverSocket, &clients);
-        fd_max = serverSocket;
+        FD_ZERO(&fds);
+        FD_SET(listenSock, &fds);
+        fd_max = listenSock;
 
         while(1)
         {
-            cpy_clients = clients;
+            readSet = fds;
+            writeSet = fds;
+
             timeout.tv_sec = 5;
             timeout.tv_usec = 5000;
 
-            if ((fd_num = select(fd_max + 1, &cpy_clients, 0, 0, &timeout)) == -1)
+            if ((fd_num = select(fd_max + 1, &readSet, &writeSet, 0, &timeout)) == -1)
                 break;
 
             if (fd_num == 0)
                 continue;
 
+            if (FD_ISSET(listenSock, &readSet))
+            {
+                adr_sz = sizeof(clientAddr);
+                clientSock = accept(listenSock, (struct sockaddr*)&clientAddr, &adr_sz);
+                fcntl(clientSock, F_SETFL, O_NONBLOCK);
+                FD_SET(clientSock, &fds);
+                if (fd_max < clientSock)
+                    fd_max = clientSock;
+                std::cout << "connected client: " << clientSock << std::endl;
+            }
+
             for (int i = 0; i < fd_max + 1; ++i)
             {
-                if (FD_ISSET(i, &cpy_clients))
+                if (FD_ISSET(i, &readSet))
                 {
-                    if (i == serverSocket)
+                    nread = read(i, buf, BUF_SIZE);
+                    if (nread == 0)
                     {
-                        adr_sz = sizeof(clientAddress);
-                        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &adr_sz);
-                        FD_SET(clientSocket, &clients);
-                        if (fd_max < clientSocket)
-                            fd_max = clientSocket;
-                        std::cout << "connected client: " << clientSocket << std::endl;
+                        FD_CLR(i, &fds);
+                        close(i);
+                        std::cout << "closed client: " << i << std::endl;
+                        continue;
                     }
                     else
                     {
-                        nread = read(i, buf, BUF_SIZE);
-                        if (nread == 0)
-                        {
-                            FD_CLR(i, &clients);
-                            close(i);
-                            std::cout << "closed client: " << i << std::endl;
-                        }
-                        else
-                        {
-                            write(i, buf, nread);
-                        }
+                        write(i, buf, nread);
                     }
-                }
+                }                
             }
         }
 
-        close(serverSocket);
+        close(listenSock);
     }
 };
 
