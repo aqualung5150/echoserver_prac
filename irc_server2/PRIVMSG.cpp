84db9f2984974100ea0b22a9fa04781ddc04d4dc
Seungjoon Choi <tmdwns0845@naver.com>
2023-05-29 16:43:10 +0900

    Parse multiple target on PRIVMSG

--- irc_server2/PRIVMSG.cpp
+++ irc_server2/PRIVMSG.cpp
@@ -10,37 +10,51 @@ void Command::PRIVMSG()
 
     std::string reply = ":" + _sender->getNick() + "!" + _sender->getUsername() + "@" + _sender->getIP() + " PRIVMSG " + _params[0] + " :" + _trailing + "\r\n";
 
-    // Msg to channel
-    if (_params[0][0] == '#')
+    //init parameters
+    std::vector<std::string> target;
+    std::string buf;
+    std::stringstream stream;
+    stream.str(_params[0]);
+    while (std::getline(stream, buf, ','))
     {
-        // if( No such cahnnel - 403 )
-        //:irc.local 403 zzz #mmmm :No such channel
-        if (_server->getChannel(_params[0]) == NULL)
-        {
-            sendReply(_sender->getSocket(), ERR_NOSUCHCHANNEL(_server->getName(), _sender->getNick(), _params[0]));
-            return;
-        }
-
-        // if( Not join the channel - ERR_CANNOTSENDTOCHAN - 404 )
-        //:irc.local 404 zzz #yyyy :You cannot send external messages to this channel whilst the +n (noextmsg) mode is set.
-        if (!_sender->isJoined(_params[0]))
-        {
-            sendReply(_sender->getSocket(), ERR_CANNOTSENDTOCHAN(_server->getName(), _sender->getNick(), _params[0]));
-            return;
-        }
-
-        // Send message every user in this channel except _sender
-        _server->getChannel(_params[0])->sendReply(reply, _sender);
-        return;
+        target.push_back(buf);
+        buf.clear();
     }
 
-    // No such nick - ERR_NOSUCHNICK - 401
-    if (_server->getUser(_params[0]) == NULL)
+    for (std::vector<std::string>::iterator it = target.begin(); it != target.end(); ++it)
     {
-        sendReply(_sender->getSocket(), ERR_NOSUCHNICK(_server->getName(), _sender->getNick(), _params[0]));
-        return ;
-    }
+        // Msg to channel
+        if ((*it)[0] == '#')
+        {
+            // if( No such cahnnel - 403 )
+            //:irc.local 403 zzz #mmmm :No such channel
+            if (_server->getChannel(*it) == NULL)
+            {
+                sendReply(_sender->getSocket(), ERR_NOSUCHCHANNEL(_server->getName(), _sender->getNick(), *it));
+                continue;
+            }
 
-    // Msg to user
-    sendReply(_server->getUser(_params[0])->getSocket(), reply);
+            // if( Not join the channel - ERR_CANNOTSENDTOCHAN - 404 )
+            //:irc.local 404 zzz #yyyy :You cannot send external messages to this channel whilst the +n (noextmsg) mode is set.
+            if (!_sender->isJoined(*it))
+            {
+                sendReply(_sender->getSocket(), ERR_CANNOTSENDTOCHAN(_server->getName(), _sender->getNick(), *it));
+                continue;
+            }
+
+            // Send message every user in this channel except _sender
+            _server->getChannel(*it)->sendReply(reply, _sender);
+            continue;
+        }
+
+        // No such nick - ERR_NOSUCHNICK - 401
+        if (_server->getUser(*it) == NULL)
+        {
+            sendReply(_sender->getSocket(), ERR_NOSUCHNICK(_server->getName(), _sender->getNick(), *it));
+            return ;
+        }
+
+        // Msg to user
+        sendReply(_server->getUser(*it)->getSocket(), reply);
+    }
 }
